1f0e6c7288012d5ae2f8113272ea14cd275727c6
Jacob Alamparambil <jacobsfo@gmail.com>
2020-06-07 21:57:05 -0700

    header

--- source/main.c
+++ source/main.c
@@ -13,33 +13,44 @@ unsigned char pause = 0;
 unsigned char led0_output = 0x00;
 unsigned char led1_output = 0x00;
 #include <avr/io.h>
+#include "timer.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
 #include "scheduler.h"
-#include "keypadsm1.h"
-#include "pause.h"
+#include "lcd.h"
+#endif
+//#include "keypadsm.h"
+#include "keytolcd.h"
 #include "toggleLED0.h"
 #include "toggleLED1.h"
 #include "display.h"
-#include "timer.h"
+//#include "timer.h"
 //const char SMStart = -1;
 int main(void) {
     /* Insert DDR and PORT initializations */
 //DDRA = 0x00; PORTA = 0xFF;
-DDRC = 0xF0; PORTC = 0x0F;
 DDRB = 0xFF; PORTB = 0x00;
-    /* Insert your solution below */
-static task tasks1;
-task *tasks[] = { &tasks1}; 
-const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-const char SMStart = -1;
+DDRD = 0xFF; PORTD = 0x00;
+// state = Start;
+//const unsigned char * tmpA = "hello";
+//LCD_init();
+//LCD_ClearScreen();
+//LCD_DisplayString(1, "Hell World");
+//TimerSet(100);
+   /* Insert your solution below */
+
+    	static task tasks1;
+    	task *tasks[] = { &tasks1}; 
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+	const char SMStart = -1;
 //PORTB = 0x01;
 //unsigned char i=0;
+ // LCD_init();
   tasks1.state = SMStart;
-  tasks1.period = 10;
+  tasks1.period = 1000;
   tasks1.elapsedTime = tasks1.period;
-  tasks1.TickFct = &Keypad;
+  tasks1.TickFct = &LCD;
+
 /*  
   tasks2.state = SMStart;
   tasks2.period = 500;
@@ -56,17 +67,20 @@ const char SMStart = -1;
   tasks4.elapsedTime = tasks4.period;
   tasks4.TickFct = &display;
 */
-  TimerSet(10);
+  
+TimerSet(1000);
   TimerOn(); 
 unsigned short i;
 
  while (1) {
-for (i = 0; i < numTasks; i++) { // Heart of the scheduler code
-     if ( tasks[i]->elapsedTime == tasks[i]->period ) { // Ready
+	for (i = 0; i < numTasks; i++)
+  { // Heart of the scheduler code
+     		if ( tasks[i]->elapsedTime == tasks[i]->period )
+		    { // Ready
         tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
         tasks[i]->elapsedTime = 0;
-     }
-     tasks[i]->elapsedTime += 10;
+		    }
+     tasks[i]->elapsedTime += 1000;
   }
 
 while(!TimerFlag);
@@ -74,6 +88,7 @@ TimerFlag = 0;
 
 
 
-}
+	}
 return 0;
+
 }
